9ecc0503cc9f1cdb1813a92c50fa1e4f75b6c8eb
kimddonggyu <dkkim1004@hanmail.net>
2021-07-24 10:13:48 +0900

    hot reload 설정

--- client.jsx
+++ client.jsx
@@ -1,2 +1,5 @@
 const React = require('react');
 const ReactDom = require('react-dom');
+const WordRelay = require('./WordRelay.jsx');
+
+ReactDom.render(<WordRelay/>, document.querySelector('#root'));
